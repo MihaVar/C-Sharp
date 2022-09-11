b88fb5327bc7bcd8386bdbbe0b0d40aa35b61260
MihaVar <82413122+MihaVar@users.noreply.github.com>
2022-09-11 11:27:24 +0300

    Update PZ1.cs

--- PZ1.cs
+++ PZ1.cs
@@ -6,15 +6,15 @@ class Program
     {
         int N, count = 0;
         Console.WriteLine("Enter amount of elements in array: ");
-        N = int.Parse(Console.ReadLine());
+        N = int.Parse(Console.ReadLine()); //вводимо кількість елементів в масиві
         Console.WriteLine("Enter array elements: " + "\n");
-        int[] arr = new int[N];
-        for (int i = 0; i < arr.Length; i++)
+        int[] arr = new int[N]; //створення масиву з розміром, який ми вказали
+        for (int i = 0; i < arr.Length; i++) //заповнення масиву
         {
             arr[i] = int.Parse(Console.ReadLine());
-            if (arr[i] < 0) count++;
+            if (arr[i] < 0) count++; //запис елементів, що менше нуля
         }
-        Console.WriteLine("Amount of negative elements: " + count);
+        Console.WriteLine("Amount of negative elements: " + count); //вивід елементів менше нуля
         Console.WriteLine("Press any button to quit from program...");
         Console.ReadKey();
     }
