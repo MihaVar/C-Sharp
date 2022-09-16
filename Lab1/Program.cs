85dbd66fe44c07e097790a7846052aa4b86fffbb
MihaVar <82413122+MihaVar@users.noreply.github.com>
2022-09-16 15:30:06 +0300

    Lab1/2

---
+++ Lab1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double R1 = 0;
+            double R2 = 0;
+            double R = 0;
+            bool isR1ok = false;
+            bool isR2ok = false;
+
+            do
+            {
+                Console.WriteLine("Введіть опір першого резистора (R1): ");
+                string strR1 = Console.ReadLine();
+                isR1ok = double.TryParse(strR1, out R1);
+                if (!isR1ok || R1 < 0)
+                {
+                    Console.WriteLine("Неправильно введений опір!");
+                }
+            } while (!isR1ok || R1 < 0);
+
+            do
+            {
+                Console.WriteLine("Введіть опір другого резистора (R2): ");
+                string strR2 = Console.ReadLine();
+                isR2ok = double.TryParse(strR2, out R2);
+                if (!isR2ok || R2 < 0)
+                {
+                    Console.WriteLine("Неправильно введений опір!");
+                }
+            } while (!isR2ok || R2 < 0);
+
+            R = (R1 * R2) / (R1 + R2);
+            Console.WriteLine("Опір двох паралельно з'єднаних резисторів: " + Math.Round(R, 2));
+            Console.WriteLine("Натисніть будь-яку клавішу для того, щоб продовжити...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nn = 0;
+            double nk = 0;
+            double sum = 0;
+            bool isNNOk = false;
+            bool isNKOk = false;
+
+            do
+            {
+                Console.WriteLine("Ввелідь змінну nn: ");
+                string strnn = Console.ReadLine();
+                isNNOk = double.TryParse(strnn, out nn);
+                if (!isNNOk || nn < 0)
+                {
+                    Console.WriteLine("Неправильно задана змінна!");
+                }
+            } while (!isNNOk || nn < 0);
+
+            do
+            {
+                Console.WriteLine("Введіть змінну nk: ");
+                string strnk = Console.ReadLine();
+                isNKOk = double.TryParse(strnk, out nk);
+                if (!isNKOk || nk < 0)
+                {
+                    Console.WriteLine("Неправильно задана змінна!");
+                }
+            } while (!isNKOk || nk < 0);
+
+            for (double k = nn; k < nk; k++)
+            {
+                sum += ((Math.Pow(nk, 2) - 1) / (Math.Pow(-1, nk + 1) * Math.Pow(nk, 2) + 7));
+            }
+            Console.WriteLine("Сума ряду: " + sum);
+            Console.WriteLine("Натисніть будь-яку клавішу, щоб продовжити...");
+            Console.ReadKey();
+        }
+    }
+}
