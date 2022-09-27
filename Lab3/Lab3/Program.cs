832ec9a8acb1409322b460acb36c8927268fdbe3
MihaVar <mykhailvarych@gmail.com>
2022-09-27 21:05:36 +0300

    Lab3/4

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            bool isNOk = false;
+            bool isNtrue;
+            
+            do
+            {
+                Console.WriteLine("Ввелідь змінну n: ");
+                string strn = Console.ReadLine();
+                isNOk = int.TryParse(strn, out n);
+                if (!isNOk)
+                {
+                    Console.WriteLine("Неправильно задана змінна!");
+                }
+            } while (!isNOk);
+
+            if (n % 2 == 0 && (n-10) > 0) //перевірка на те, чи є число парним та двозначним
+            {
+                isNtrue = true;
+                
+            }
+            else
+            {
+                isNtrue = false;
+                
+                
+            }
+            Console.WriteLine(isNtrue);
+            Console.WriteLine("Натисніть будь-яку клавішу, щоб продовжити...");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main()
+    {
+
+        int n;
+        bool isNOk = false;
+        do //введення розміру масиву
+        {
+            Console.WriteLine("Введіть розмір масиву (N): ");
+            string strn = Console.ReadLine();
+            isNOk = int.TryParse(strn, out n);
+            if (!isNOk || n < 0)
+            {
+                Console.WriteLine("Неправильне значення!");
+            }
+        } while (!isNOk || n < 0);
+        int[] arr = new int[n];
+        Console.Write("Введіть елементи масиву: ");
+        Console.Write("\n");
+        for (int i = 0; i < n; i++) //введення елементів масиву
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+        for (int i = 0; i <= n - 1; i++) //вивід непарних
+        {
+            if (arr[i] % 2 != 0)
+            {
+                Console.Write(arr[i]);
+                Console.Write(' ');
+            }
+        }
+        for (int i = 0; i <= n - 1; i++) //вивід парних
+        {
+            if (arr[i] % 2 == 0)
+            {
+                Console.Write(arr[i]);
+                Console.Write(' ');
+            }
+        }
+        Console.WriteLine("\n");
+        Console.WriteLine("Натисніть будь-яку клавішу для того, щоб продовжити...");
+        Console.ReadKey();
+    }
+}
+
